7858a436702045baff4cc99d008e8285d923c517
Yazid Ahlam Syaefullah <yazidas1604@gmail.com>
2025-12-03 12:32:12 +0700

    windows is unstable

---
+++ build/CMakeFiles/ather.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ather.dir/link.d"
+  "CMakeFiles/ather.dir/main.c.o"
+  "CMakeFiles/ather.dir/main.c.o.d"
+  "ather"
+  "ather.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
